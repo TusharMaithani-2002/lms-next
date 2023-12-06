638c67d22a1d1627b8473f06b67a4fd7ebcf5fc7
Tushar Maithani <202051194@iiitvadodara.ac.in>
2023-12-06 15:45:58 +0530

    added navbar and mobile sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,20 @@
 import { ReactNode } from "react";
 import SideBar from "./_components/SideBar";
+import NavBar from "./_components/NavBar";
 
-const DashBoardLayout = ({children}:{children:ReactNode}) => {
-    return (
-        <div className="h-full">
-            <div className="hidden md:flex w-56 flex-clo fixed inset-y-0 z-50">
-                <SideBar />
-            </div>
-            {children}
+const DashBoardLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="h-full">
+        <div className="h-[80px] fixed inset-y-0 w-full z-50">
+            <NavBar />
         </div>
-    )
-}
+      <div className="hidden md:flex w-56 flex-clo fixed inset-y-0 z-50">
+        <SideBar />
+      </div>
+      {/* we are creating this main as our side bar is 56units wide and we want are children to start after the sidebar */}
+      <main className="md:pl-56 h-full">{children}</main>
+    </div>
+  );
+};
 
 export default DashBoardLayout;
